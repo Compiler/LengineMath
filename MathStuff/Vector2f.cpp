c1d78e537e25b9f8b186b100af48c51d291059cd
Luke <lr98lr@aol.com>
2016-10-16 21:05:46 -0400

    more vector functions

--- MathStuff/Vector2f.cpp
+++ MathStuff/Vector2f.cpp
@@ -7,18 +7,38 @@ Vector2f::Vector2f(): x(0), y(0){
 
 }
 
+Vector2f::Vector2f(float xd, float yd){
+	x = xd;
+	y = yd;
+}
+
 
 Vector2f Vector2f::projectOnto(Vector2f vec){
 
+
+
+}
+
+
+void Vector2f::normalize(){
+	x = x * magnitude();
+	y = y * magnitude();
+}
+
+
+Vector2f Vector2f::getNormalized(){
+	return Vector2f(magnitude() * x, magnitude() * y);
 }
 
 float Vector2f::angleBetween(Vector2f vec){
 
 }
 
-Vector2f Vector2f::normalize(){
-	
+
+float Vector2f::magnitude(){
+	return sqrtf(pow(x, 2) + pow(y, 2));
 }
+
 
 
 Vector2f::~Vector2f(){
--- MathStuff/Vector2f.h
+++ MathStuff/Vector2f.h
@@ -1,15 +1,22 @@
 #pragma once
+
+#include "math.h"
+
 class Vector2f{
 public:
 	Vector2f();
+	Vector2f(float xd, float yd);
 
 
 
+
+	float angleBetween(Vector2f vec);
+	float magnitude();
 
 	Vector2f projectOnto(Vector2f vec);
-	float angleBetween(Vector2f vec);
-	Vector2f normalize();
+	Vector2f getNormalized();
 
+	void normalize();
 
 
 	float x, y;
